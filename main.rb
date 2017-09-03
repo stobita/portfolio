20e01fc3e3bbb47ea8b6aa17a0f89f8276788c4b
stobita <stobita@example.com>
2017-09-03 22:06:49 +0900

    add pages

--- main.rb
+++ main.rb
@@ -5,6 +5,28 @@ get '/' do
     erb :index
 end
 
+get '/design' do
+    erb :design
+end
+
+get '/products' do
+    erb :products
+end
+
+get '/history' do
+    erb :history
+end
+
+get '/design/portfolio-sample' do
+    erb :'design/portfolio-sample'
+end
+
 get '/stylesheets/main.css' do
   scss :'stylesheets/main'
+end
+get '/stylesheets/app.css' do
+  scss :'stylesheets/app'
+end
+get '/stylesheets/ps1.css' do
+  scss :'stylesheets/ps1'
 end
